4f9946b84813aaf7f1ea2c7b84d324bb9ebf8f7f
Rob van der Linde <rob@catalyst.net.nz>
2017-04-01 10:55:25 +1300

    Fixes too long line in test

--- tests/unit/guests/test_base.py
+++ tests/unit/guests/test_base.py
@@ -64,7 +64,8 @@ class TestGuest:
             name = 'dummy'
         lxd_container = unittest.mock.Mock()
         lxd_container.execute.return_value = ('ok', 'ok', '')
-        password = '$6$cGzZBkDjOhGW$6C9wwqQteFEY4lQ6ZJBggE568SLSS7bIMKexwOD39mJQrJcZ5vIKJVIfwsKOZajhbPw0.Zqd0jU2NDLAnp9J/1'
+        password = '$6$cGzZBkDjOhGW$6C9wwqQteFEY4lQ6ZJBggE568SLSS7bIMKexwOD'\
+                   '39mJQrJcZ5vIKJVIfwsKOZajhbPw0.Zqd0jU2NDLAnp9J/1'
         guest = DummyGuest(lxd_container)
         guest.create_user('usertest', password=password)
         assert lxd_container.execute.call_count == 1
