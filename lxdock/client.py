6c346b2346ea9b7a17e227e120f77d2ef35922ed
Shuhao Wu <shuhao@shuhaowu.com>
2018-02-16 20:45:34 -0500

    lxdock + snap
    Upstream has a bug: https://github.com/lxc/pylxd/issues/257
    
    It is fixed in 2.2.5, which is yet to be released. This commit fixes
    this situation within lxdock.

--- lxdock/client.py
+++ lxdock/client.py
@@ -1,6 +1,17 @@
+import os
+from pkg_resources import parse_version as ver
+
 import pylxd
 
 
 def get_client():
     """ Returns a PyLXD client to be used to orchestrate containers. """
+    # Fixed a bug upstream: https://github.com/lxc/pylxd/issues/257
+    lxd_dir_not_set = "LXD_DIR" not in os.environ
+    snap_socket_exists = os.path.exists('/var/snap/lxd/common/lxd/unix.socket')
+    insufficient_version = ver(pylxd.__version__) < ver("2.2.5")
+
+    if lxd_dir_not_set and snap_socket_exists and insufficient_version:
+      os.environ["LXD_DIR"] = "/var/snap/lxd/common/lxd"
+
     return pylxd.Client()
